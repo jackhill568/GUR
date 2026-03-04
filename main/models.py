3218f6d04617a5335d98c7d6d75aee58795c33d0
jack <jackhill9090@gmail.com>
2026-03-04 17:09:56 +0000

    added population script and made some updates

--- main/models.py
+++ main/models.py
@@ -12,7 +12,7 @@ class User(models.Model):
 
     first_name  = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
-    nickname = models.CharField(max_length=128)
+    nickname = models.CharField(max_length=128, unique=True)
 
     email = models.CharField(max_length=128)
     password = models.CharField(max_length=128)
