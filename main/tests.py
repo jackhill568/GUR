04557dd94e9b6b05b4663d5c2e1bb2ced2e7f767
jack <jackhill9090@gmail.com>
2026-03-07 18:55:58 +0000

    added one test

--- main/tests.py
+++ main/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 
 from main.models import Category,Ingredient,Recipe,RecipeIngredients,Review,User
 
@@ -54,6 +55,56 @@ class IngredientMethodTests(TestCase):
     
 
 
+class HomeViewTests(TestCase):
+
+    def test_home_with_no_recipe(self):
+
+        response = self.client.get(reverse('main:home'))
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(response, "No recipes can be found")
+
+        self.assertQuerysetEqual(response.context["recipe"], [])
 
 
+    def test_home_recipe_of_week(self):
+
+
+
+
+
+class HomeViewTests(TestCase):
+
+    def test_home_with_no_recipe(self):
+
+        response = self.client.get(reverse('main:home'))
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(response, "No recipes can be found")
+
+        self.assertQuerysetEqual(response.context["recipe"], [])
+
+
+    def test_home_recipe_of_week(self):
+
+
+
+class HomeViewTests(TestCase):
+
+    def test_home_with_no_recipe(self):
+
+        response = self.client.get(reverse('main:home'))
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(response, "No recipes can be found")
+
+        self.assertQuerysetEqual(response.context["recipe"], [])
+
+
+    def test_home_recipe_of_week(self):
+
+        pass
 
