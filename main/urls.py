9bec80320e263ab2ea13714c5b29da3ab4f1eabe
jack <jackhill9090@gmail.com>
2026-03-11 11:39:44 +0000

    some setup and some templates

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,10 @@ app_name = 'GUR'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('', views.add_recipe, name='upload a recipe'),
+    path('register/', views.register, name='register'),
+    path('recipe/search', views.register, name='search'),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Home page")
+from django.shortcuts import redirect
+from django.urls import reverse
 
+from django.contrib.auth.decorators import login_required
+
+def home(request):
+    return render(request, 'main/home.html')
+
+@login_required
+def add_recipe(request):
+    return HttpResponse("upload recipe")
+
+def register(request):
+    return HttpResponse("sign up")
+
+def user_login(request):
+    return HttpReponse("login")
+
+@login_required
+def user_logout(request):
+    return HttpResponse("logout")
 
 
