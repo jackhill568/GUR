7911260d5a983d9df8e2f21ee300081720c1fbae
jack <jackhill9090@gmail.com>
2026-03-17 21:17:36 +0000

    searching mostly complete

--- main/search_indexes.py
+++ main/search_indexes.py
@@ -1,6 +1,6 @@
 import datetime
 from haystack import indexes
-from main.models import User, Recipe, Category, Review
+from main.models import User, Recipe, Review
 
 class UserIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
@@ -12,4 +12,34 @@ class UserIndex(indexes.SearchIndex, indexes.Indexable):
     def get_model(self):
         return User
 
+class RecipeIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    
+    name = indexes.CharField(model_attr='name')
+    description = indexes.CharField(model_attr='description')
+    method = indexes.CharField(model_attr='method')
+    ingredients = indexes.MultiValueField()
 
+    def get_model(self):
+        return Recipe
+
+    def prepare_ingredients(self, obj):
+        return [
+            ri.ingredient.name
+            for ri in obj.recipeingredients_set.all()
+        ]    
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
+
+class ReviewIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    
+    rating = indexes.CharField(model_attr='rating')
+    description = indexes.CharField(model_attr='description')
+
+    def get_model(self):
+        return Review
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
